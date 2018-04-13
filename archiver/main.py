17d63a7ab08892a7a7851203fa71921d15557837
fletchgraham <sfletchgraham@gmail.com>
2018-04-12 20:38:10 -0400

    users now enter their entries with a text editor rather than straight into the command prompt.

--- archiver/main.py
+++ archiver/main.py
@@ -1,7 +1,6 @@
 import click
 import os
 import io
-import sqlite3
 import sys
 from datetime import datetime
 
@@ -39,17 +38,37 @@ class Archivist:
         # change the contents of config file to new location
         # update self.location variable
 
-    def write(self, entry):
-        
+    def write(self):
+
+        # Get the text editor from the shell, otherwise default to Vim
+        editor = os.environ.get('EDITOR','vim')
+
+        # define the locations of the archive file and the temp file
         archive_path = os.path.join(self.location, 'archive.txt')
+        temp_path = os.path.join(self.location, 'temp_entry.txt')
+
+        # create the temp file with a helpful message
+        with open(temp_path, 'w') as temp:
+            temp.write('')
+
+        # open the temp file with an editor so the user can write their entry
+        os.system(editor + ' ' + temp_path)
+
+        # read the entry from the temp file once the user has saved their entry
+        with open(temp_path, 'r') as temp:
+            entry = temp.read()
+
+        # nuke all any temp files that were created by archiver or the text editor
+        for file in os.listdir(self.location):
+            if 'temp_entry' in file:
+                os.remove(os.path.join(self.location, file))
+
+        # get the current time
         timestamp = datetime.now().isoformat(timespec='seconds')
-        
+
+        # write the whole thing to the archive
         with open(archive_path, 'a') as archive_file:
-            archive_file.write(
-                '\n\n||\n\n' + timestamp + '\n\n|\n\n' + entry
-                )
-        # open the file for appending, append the current date and time
-        # followed by the entry
+            archive_file.write('\n\n||\n\n' + timestamp + '\n\n|\n\n' + entry)
 
     def read(self, date):
         pass
@@ -58,8 +77,6 @@ class Archivist:
         # close the db
         # echo the result in a pretty way
         
-        
-
 
 @click.group()
 def archiver():
@@ -70,11 +87,11 @@ def archiver():
 def write():
     # spin up the archivist object
     archivist = Archivist()
-    # collect the entry from the user
-    entry = input('Create your entry >>> ')
-    # run the write method of the archivist with the entry
-    archivist.write(entry)
+    # run the write method of the archivist
+    archivist.write()
+    # tell the user what happened
     click.echo('Your entry was written to the archive.')
+
 
 
 @archiver.command()
