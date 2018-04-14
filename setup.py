be1a13f3f640a842425d0d51709a6e6902e8b958
fletchgraham <sfletchgraham@gmail.com>
2018-04-13 23:51:36 -0400

    add pycryptodome as a dependency.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='archiver',
     version='1.0',
     packages=['archiver'],
-    install_requires=['Click'],
+    install_requires=['Click', 'pycryptodome'],
     entry_points='''
         [console_scripts]
         arc=archiver.main:archiver
